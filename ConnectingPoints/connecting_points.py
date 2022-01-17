d1a93efd4b8ac0540a32d11dd15a838c1e9d6e75
Michael Ehrlichman <ehrlichman.m@gmail.com>
2022-01-17 15:02:00 -0800

    Minimum total length of edges connecting points on 2D graph

---
+++ ConnectingPoints/connecting_points.py
@@ -0,0 +1,49 @@
+#Uses python3
+import sys
+import math
+
+def findMin(cost):
+    minimum = cost[0]
+    minimum_i = 0
+    for i in range(1,len(cost)):
+        if cost[i] < minimum:
+            minimum = cost[i]
+            minimum_i = i
+    return minimum_i
+
+def update_and_delete(inew,x,y,cost):
+    for i,(xf,yf) in enumerate(zip(x,y)):
+        if i != inew:
+            costnew = ( (x[inew]-xf)**2 + (y[inew]-yf)**2 )**0.5 
+            if costnew < cost[i]:
+                cost[i] = costnew
+    del x[inew]
+    del y[inew]
+    del cost[inew]
+
+def minimum_distance(x, y):
+    #Prim's Algorithm using cost as the array-based priority queue.
+    #The graph is dense, a potential edge exists between every other vertex, so O(V^2) wins over O(E log V).
+    #All we are interested in for this problem is the total length of edges.
+    cost = [float('inf') for i in range(len(x))]
+
+    #Start with element 0
+    result = 0
+    cost[0] = 0
+    inew = 0
+    update_and_delete(0,x,y,cost)
+
+    while x:
+        inew = findMin(cost)
+        result += cost[inew]
+        update_and_delete(inew,x,y,cost)
+
+    return result
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n = data[0]
+    x = data[1::2]
+    y = data[2::2]
+    print("{0:.9f}".format(minimum_distance(x, y)))
