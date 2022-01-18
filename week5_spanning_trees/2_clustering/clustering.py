e2e36f2966539abd9de523c75b3edbcf90e58ad5
Michael Ehrlichman <ehrlichman.m@gmail.com>
2022-01-17 23:27:44 -0800

    updates

--- week5_spanning_trees/2_clustering/clustering.py
+++ week5_spanning_trees/2_clustering/clustering.py
@@ -1,37 +1,58 @@
 #Uses python3
 import sys
-import math
 
 class Set():
     def __init__(self,x,y):
-        self.parent = [i for i in range(len(x))]
-        self.rank = [0 for i in range(len(x))]
+        self.n = len(x)
+        self.parent = [i for i in range(self.n)]
+        self.ndisjoint = self.n
+        self.rank = [0 for i in range(self.n)]
         self.x = x
         self.y = y
         self.d = self.makeEdges(x,y)
+        self.d.sort(key=lambda x:x[2])
 
     def makeEdges(self,x,y):
-        dists = [[None for j in range(len(x))] for i in range(len(x))]
-        for ji, xi,yi in enumerate(zip(x,y)):
-            for jf, xf,yf in enumerate(zip(x,y)):
+        dists = [None for i in range(len(x)**2)]
+        i = 0
+        for ji,(xi,yi) in enumerate(zip(x,y)):
+            for jf,(xf,yf) in enumerate(zip(x,y)):
                 if ji != jf:
-                    self.d[ji][jf] = ( (xi-xf)^2 + (yi-yf)^2 )^0.5
+                    dists[i] = [ji,jf,( (xi-xf)**2 + (yi-yf)**2 )**0.5]
                 else:
-                    self.d[ji][jf] = float('inf')
+                    dists[i] = [ji,jf,float('inf')]
+                i += 1
+        return dists
 
-    def union(i_id,j_id):
-        if rank[i_id] > rank[j_id]:
-            parent[j_id] = i_id
+    def Find(self,i):
+        while i != self.parent[i]:
+            i = self.parent[i]
+        return i
+
+    def Union(self,i,j):
+        i_id = self.Find(i)
+        j_id = self.Find(j)
+        if i_id == j_id:
+            return
+        if self.rank[i_id] > self.rank[j_id]:
+            self.parent[j_id] = i_id
         else:
-            parent[i_id] = j_id
-            if rank[i_id] == rank[j_id]:
-                rank[j_id] += 1
+            self.parent[i_id] = j_id
+            if self.rank[i_id] == self.rank[j_id]:
+                self.rank[j_id] += 1
+        self.ndisjoint -= 1
         
 
 def clustering(x, y, k):
+    # Adapt Kruskal's algorithm
     mySet = Set(x,y)
-    
-    return -1.
+    for ix,iy,d in mySet.d:
+        if mySet.ndisjoint == k-1:
+            break
+        mySet.Union(ix,iy)
+    #print(mySet.d)
+    #print(mySet.parent,mySet.ndisjoint,d)
+    return d
 
 
 if __name__ == '__main__':
